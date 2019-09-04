52e65dd6c3307afd4eefe3ef9cc531fe013f1b8f
yuweichen1008 <yuweiichen@gmail.com>
2019-09-04 08:30:56 +0800

    add django_table

---
+++ mysite/tutorial/tables.py
@@ -0,0 +1,18 @@
+# https://django-tables2.readthedocs.io/en/latest/pages/tutorial.html
+import django_tables2 as tables
+from .models import Person
+data = [
+    {"name": "Bradley"},
+    {"name": "Stevie"},
+]
+
+class NameTable(tables.Table):
+    name = tables.Column()
+
+table = NameTable(data)
+
+class PersonTable(tables.Table):
+    class Meta:
+        model = Person
+        template_name = "django_tables2/bootstrap.html"
+        fields = ("name", )
--- mysite/tutorial/views.py
+++ mysite/tutorial/views.py
@@ -1,11 +1,14 @@
+from django_tables2 import SingleTableView
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Person
+from .tables import PersonTable
+
 # Create your views here.
 def index(request):
     return render(request, 'tutorial/index.html')
 
-
-class PersonListView(ListView):
+class PersonListView(SingleTableView):
     model = Person
+    table_class = PersonTable
     template_name = 'tutorial/people.html'
