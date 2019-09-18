d8d56d1963202f98fc1a800358dc75dd315ac8aa
yuweichen1008 <yuweiichen@gmail.com>
2019-09-18 20:59:11 +0800

    add login page

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'tutorial.apps.TutorialConfig',
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
+    # 'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'blog-home'
