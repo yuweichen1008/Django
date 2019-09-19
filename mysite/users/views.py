ea8d70a175f9a3ab2cbba89d479f8be05749b743
yuweichen1008 <yuweiichen@gmail.com>
2019-09-20 07:06:57 +0800

    update profile

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
@@ -10,7 +10,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'{username} is created! Now Please log in')
+            messages.success(request, f'Congrats! {username} is now created! Check your profile.')
             return redirect('login')
     else:
         form = UserRegisterForm()
@@ -18,4 +18,21 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request,'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, 
+                                   request.FILE,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your Profile has been updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'u_form' : u_form,
+        'p_form' : p_form
+    }
+    return render(request,'users/profile.html', context)
